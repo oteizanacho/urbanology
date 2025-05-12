eb0af84d2a959dd9aec0dc1f9b7aba823aacadb4
oteizanacho <oteizanacho@gmail.com>
2025-05-12 02:06:57 -0300

    styles: botones

--- scripts.js
+++ scripts.js
@@ -19,3 +19,22 @@ window.addEventListener("scroll", function () {
     }
 });
 */
+
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("#navbar a");
+
+  function updateActiveLink() {
+    let index = sections.length;
+
+    while (--index >= 0 && window.scrollY + 100 < sections[index].offsetTop) {}
+
+    navLinks.forEach(link => link.classList.remove("active"));
+    if (index >= 0) {
+      const id = sections[index].id;
+      const activeLink = document.querySelector(`#navbar a[href="#${id}"]`);
+      if (activeLink) activeLink.classList.add("active");
+    }
+  }
+
+  window.addEventListener("scroll", updateActiveLink);
+  window.addEventListener("load", updateActiveLink);
