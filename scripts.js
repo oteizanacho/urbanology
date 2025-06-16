7e441075caa4e29db3b4f77e48b5341c8dc19ff0
oteizanacho <oteizanacho@gmail.com>
2025-06-16 20:17:44 -0300

    estilos finales

--- scripts.js
+++ scripts.js
@@ -1,50 +1,14 @@
-const btn = document.getElementById("hamburguesa");
-const menu = document.getElementById("navbar");
-const cerrar = document.getElementById("cerrar");
-const header = document.querySelector("header");
-const links = document.querySelectorAll(".nav-opcion");
-const secciones = document.querySelectorAll("section, footer");
+const hamburguesaBtn = document.getElementById("hamburguesa")
+const cerrarBtn = document.getElementById("cerrar")
+const navBar = document.querySelector("nav")
 
-// Menú hamburguesa
-btn.addEventListener("click", () => {
-  btn.style.display = "none";
-  cerrar.style.display = "block";
-  menu.style.display = "flex";
-  menu.style.flexDirection = "column";
-  menu.style.alignItems = "end";
-});
+hamburguesaBtn.addEventListener("click", () => {
+  hamburguesaBtn.style.display = "none"
+  cerrarBtn.style.display = "block"
+  navBar.style.display = "flex"
+})
 
-cerrar.addEventListener("click", () => {
-  menu.style.display = "none";
-  btn.style.display = "block";
-});
-
-// Mostrar header al hacer scroll
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    header.classList.add("visible");
-  } else {
-    header.classList.remove("visible");
-  }
-
-  // Scrollspy
-  secciones.forEach(seccion => {
-    const top = seccion.offsetTop - 100;
-    const bottom = top + seccion.offsetHeight;
-    const scroll = window.scrollY;
-
-    if (scroll >= top && scroll < bottom) {
-      const id = seccion.getAttribute("id");
-      links.forEach(link => {
-        link.classList.remove("active");
-        if (link.getAttribute("href") === `#${id}`) {
-          link.classList.add("active");
-        }
-      });
-    }
-  });
-});
-
-document.getElementById("contact-form").addEventListener("submit", e => {
-  e.preventDefault();
+cerrarBtn.addEventListener("click", () => {
+  hamburguesaBtn.style.display = "block"
+  navBar.style.display = "none"
 })
